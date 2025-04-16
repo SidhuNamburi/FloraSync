1e50b75e9e2fd9ae9413802d8c398825d4b3bfc9
rithik5k2 <veeranenirithikrao@gmail.com>
2025-04-16 22:08:00 +0530

    Initial project files push

---
+++ src/Mycomponents/Navbar.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import './Navbar.css';
+
+const Navbar = () => {
+  return (
+    <header>
+      <div className="logo">Flora Sync</div>
+      <nav>
+        <ul>
+          <li><a href="#">Home</a></li>
+          <li><a href="#">Plants</a></li>
+          <li><a href="#">Weather</a></li>
+          <li><a href="#">Projects</a></li>
+          <li><a href="#">Help</a></li>
+        </ul>
+      </nav>
+      <div className="search-container">
+        <input type="text" placeholder="Search plants..." />
+        <button>🔍</button> {/* Using magnifying glass emoji */}
+      </div>
+    </header>
+  );
+};
+
+export default Navbar;
